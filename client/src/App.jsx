d42931678e148ac00d791edd06f11bcddef99150
emilbankov <emilbankov17d@gmail.com>
2025-01-25 01:06:34 +0200

    Added browser router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
-import { useEffect } from "react";
+import { Routes, Route, useLocation } from 'react-router-dom';
+
+import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-    
+
 
     return (
         <>
-            <Home />
+            <Header />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/login' element={<Login />} />
+            </Routes>
+
+            <Footer />
         </>
     );
 }
