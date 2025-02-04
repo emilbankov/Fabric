239f512a0c09589f92339a269465cb6f0979afcf
emilbankov <emilbankov17d@gmail.com>
2025-02-04 03:14:49 +0200

    Updated authentication service

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,14 +1,41 @@
 import { get, post } from '../lib/request.js';
 
-const baseUrl = 'http://localhost:8081/users';
+const baseUrl = 'https://tshirt-latest.onrender.com/users';
 
-export const login = (email, password) => post(`${baseUrl}/login`, {
-    email,
-    password
-});
+const getDeviceInfo = () => {
+    const screenResolution = `${window.screen.width}x${window.screen.height}`;
+    const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    const plugins = JSON.stringify(
+        Array.from(navigator.plugins).map(plugin => ({
+            name: plugin.name,
+            description: plugin.description,
+            filename: plugin.filename,
+        }))
+    );
+    const hardwareConcurrency = navigator.hardwareConcurrency;
+    const deviceMemory = navigator.deviceMemory;
 
-export const register = (email, password, address) => post(`${baseUrl}/register`, {
-    email,
-    password,
-    address
-});
+    return { screenResolution, timezone, plugins, hardwareConcurrency, deviceMemory };
+};
+
+export const login = (email, password) => {
+    const deviceInfo = getDeviceInfo();
+    return post(`${baseUrl}/login`, {
+        email,
+        password,
+        deviceInfo,
+    });
+};
+
+export const register = (firstName, lastName, email, telephone, address, password) => {
+    const deviceInfo = getDeviceInfo();
+    return post(`${baseUrl}/register`, {
+        firstName,
+        lastName,
+        email,
+        telephone,
+        address,
+        password,
+        deviceInfo,
+    });
+};
