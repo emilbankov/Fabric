db9c7b6ebdb0b530baddeefae5e72d4d3beb7a19
emilbankov <emilbankov17d@gmail.com>
2025-02-04 22:22:57 +0200

    Updated request js

--- client/src/lib/request.js
+++ client/src/lib/request.js
@@ -1,45 +1,62 @@
-const buildOptions = (data) => {
-    const options = {};
-
+const getDeviceInfo = () => {
+    const screenResolution = `${window.screen.width}x${window.screen.height}`;
+    const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    const hardwareConcurrency = navigator.hardwareConcurrency || 'Unknown Hardware';
+    const deviceMemory = navigator.deviceMemory || 'Unknown Memory';
+  
+    return { screenResolution, timezone, hardwareConcurrency, deviceMemory };
+  };
+  
+  const buildOptions = (data, customHeaders = {}) => {
+    const options = {
+      headers: {
+        ...customHeaders,
+      },
+    };
+  
     if (data) {
+      if (data instanceof FormData) {
+        options.body = data;
+      } else {
         options.body = JSON.stringify(data);
-        options.headers = {
-            'content-type': 'application/json'
-        };
+        options.headers['Content-Type'] = 'application/json';
+      }
     }
-
+  
+    const deviceInfo = getDeviceInfo();
+    options.headers['screenResolution'] = deviceInfo.screenResolution;
+    options.headers['timezone'] = deviceInfo.timezone;
+    options.headers['hardwareConcurrency'] = String(deviceInfo.hardwareConcurrency);
+    options.headers['deviceMemory'] = String(deviceInfo.deviceMemory);
+  
     const token = localStorage.getItem('accessToken');
-
     if (token) {
-        options.headers = {
-            ...options.headers,
-            'Authorization': `Bearer ${token}`
-        };
+      options.headers['Authorization'] = `Bearer ${token}`;
     }
-
+  
     return options;
-};
-
-const request = async (method, url, data) => {
+  };
+  
+  const request = async (method, url, data, customHeaders) => {
     const response = await fetch(url, {
-        ...buildOptions(data),
-        method,
+      ...buildOptions(data, customHeaders),
+      method,
     });
-
+  
     if (response.status === 204) {
-        return {};
+      return {};
     }
-
+  
     const result = await response.json();
-
+  
     if (!response.ok) {
-        throw result;
+      throw result;
     }
-
+  
     return result;
-};
-
-export const get = request.bind(null, 'GET');
-export const post = request.bind(null, 'POST');
-export const put = request.bind(null, 'PUT');
-export const del = request.bind(null, 'DELETE');
+  };
+  
+  export const get = request.bind(null, 'GET');
+  export const post = request.bind(null, 'POST');
+  export const put = request.bind(null, 'PUT');
+  export const del = request.bind(null, 'DELETE');
