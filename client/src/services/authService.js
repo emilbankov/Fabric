b0445a3fb1d4bba699c415c0c6584b5ee09bc215
emilbankov <emilbankov17d@gmail.com>
2025-02-04 22:23:07 +0200

    Updated services

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -2,35 +2,14 @@ import { get, post } from '../lib/request.js';
 
 const baseUrl = 'https://tshirt-latest.onrender.com/users';
 
-const getDeviceInfo = () => {
-    const screenResolution = `${window.screen.width}x${window.screen.height}`;
-    const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-    const plugins = JSON.stringify(
-        Array.from(navigator.plugins).map(plugin => ({
-            name: plugin.name,
-            description: plugin.description,
-            filename: plugin.filename,
-        }))
-    );
-    const hardwareConcurrency = navigator.hardwareConcurrency;
-    const deviceMemory = navigator.deviceMemory;
-
-    return { screenResolution, timezone, plugins, hardwareConcurrency, deviceMemory };
-};
-
 export const login = (email, password) => {
-    const deviceInfo = getDeviceInfo();
-
     return post(`${baseUrl}/login`, {
         email,
         password,
-        deviceInfo,
     });
 };
 
 export const register = (firstName, lastName, email, phoneNumber, address, password) => {
-    const deviceInfo = getDeviceInfo();
-
     return post(`${baseUrl}/register`, {
         firstName,
         lastName,
@@ -38,6 +17,5 @@ export const register = (firstName, lastName, email, phoneNumber, address, passw
         phoneNumber,
         address,
         password,
-        deviceInfo,
     });
 };
