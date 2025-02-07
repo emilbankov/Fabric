591013057b688d39075d43eb75fb21e9bb417c43
emilbankov <emilbankov17d@gmail.com>
2025-02-07 23:07:49 +0200

    Created dictionary js

---
+++ client/src/lib/dictionary.js
@@ -0,0 +1,34 @@
+export const typeTranslations = {
+    "T_SHIRT": "Тениска",
+    "LONG_T_SHIRT": "Блуза",
+    "SWEATSHIRT": "Суитчър",
+    "SHORTS": "Къси панталони",
+    "KIT": "Комплект",
+};
+
+export const modelType = {
+    "LONG_T_SHIRT": "D",
+    "SWEATSHIRT": "SW",
+    "SHORTS": "K",
+    "KIT": "KT",
+};
+
+export const categoryTranslations = {
+    "UEFA_EURO_2024": "УЕФА ЕВРО 2024",
+    "PATRIOTIC": "Патриотични",
+    "TRUCKS": "Камиони",
+    "MOVIES": "Филми",
+    "CHRISTMAS": "Коледа",
+    "FOOTBALL": "Футбол",
+    "MOTORCYCLES": "Мотори",
+    "GAME_OF_THRONES": "Гейм Аф Тронс",
+    "DOGS": "Кучета",
+    "MARTIAL_SPORTS": "Бойни спортове",
+    "MUSIC": "Музика",
+    "CARS": "Коли",
+    "HUNTING": "Лов",
+    "FISHING": "Риболов",
+    "FORMULA_1": "Формула 1",
+    "WORK": "Работни",
+    "OTHERS": "Други"
+};
