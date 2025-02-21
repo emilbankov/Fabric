6ccb587eaa8276c878a53134f92a04a887dbb2f4
emilbankov <emilbankov17d@gmail.com>
2025-02-22 00:19:46 +0200

    Added new services

--- client/src/services/ordersService.js
+++ client/src/services/ordersService.js
@@ -3,3 +3,5 @@ import { get, post, put, del } from '../lib/request.js';
 const baseUrl = 'https://tshirt-latest.onrender.com/orders';
 
 export const createOrder = async (orderData) => await post(`${baseUrl}/create`, orderData);
+export const ordersHistoryAdmin = async () => await get(`${baseUrl}/list`);
+export const orderDetails = async (orderId) => await get(`${baseUrl}/${orderId}`);
