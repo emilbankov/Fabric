a80e48237c03c8e47f38a0c34cd64974588988fb
emilbankov <emilbankov17d@gmail.com>
2025-02-21 19:42:09 +0200

    Created orders history for users

---
+++ client/src/components/Orders History/OrdersHistoryUsers.jsx
@@ -0,0 +1,234 @@
+export default function OrdersHistoryUsers() {
+    return (
+        <>
+            <div className="account-order   layout-2 left-col">
+                <div className="content_headercms_bottom" />
+                <div className="content-top-breadcum">
+                    <div className="container">
+                        <div className="row">
+                            <div id="title-content"></div>
+                        </div>
+                    </div>
+                </div>
+                <div id="account-order" className="container">
+                    <ul className="breadcrumb">
+                        <li>
+                            <a href="https://opc.webdigify.com/OPC02/OPC037_vesture/index.php?route=common/home">
+                                <i className="fa fa-home" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://opc.webdigify.com/OPC02/OPC037_vesture/index.php?route=account/account">
+                                Account
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://opc.webdigify.com/OPC02/OPC037_vesture/index.php?route=account/order">
+                                Order History
+                            </a>
+                        </li>
+                    </ul>
+                    <div className="row">
+                        <aside id="column-left" className="col-sm-3 hidden-xs">
+                            <div className="box">
+                                <div className="box-heading">Account</div>
+                                <div className="list-group">
+                                    <a
+                                        href="https://opc.webdigify.com/OPC02/OPC037_vesture/index.php?route=account/account "
+                                        className="list-group-item"
+                                    >
+                                        My Account{" "}
+                                    </a>
+                                    <a
+                                        href="https://opc.webdigify.com/OPC02/OPC037_vesture/index.php?route=account/edit "
+                                        className="list-group-item"
+                                    >
+                                        Edit Account
+                                    </a>{" "}
+                                    <a
+                                        href="https://opc.webdigify.com/OPC02/OPC037_vesture/index.php?route=account/password"
+                                        className="list-group-item"
+                                    >
+                                        Password
+                                    </a>
+                                    <a
+                                        href="https://opc.webdigify.com/OPC02/OPC037_vesture/index.php?route=account/address"
+                                        className="list-group-item"
+                                    >
+                                        Address Book
+                                    </a>{" "}
+                                    <a
+                                        href="https://opc.webdigify.com/OPC02/OPC037_vesture/index.php?route=account/wishlist "
+                                        className="list-group-item"
+                                    >
+                                        Wish List{" "}
+                                    </a>{" "}
+                                    <a
+                                        href="https://opc.webdigify.com/OPC02/OPC037_vesture/index.php?route=account/order "
+                                        className="list-group-item"
+                                    >
+                                        Order History{" "}
+                                    </a>{" "}
+                                    <a
+                                        href="https://opc.webdigify.com/OPC02/OPC037_vesture/index.php?route=account/download"
+                                        className="list-group-item"
+                                    >
+                                        Downloads{" "}
+                                    </a>
+                                    <a
+                                        href="https://opc.webdigify.com/OPC02/OPC037_vesture/index.php?route=account/recurring"
+                                        className="list-group-item"
+                                    >
+                                        Recurring payments{" "}
+                                    </a>{" "}
+                                    <a
+                                        href="https://opc.webdigify.com/OPC02/OPC037_vesture/index.php?route=account/reward "
+                                        className="list-group-item"
+                                    >
+                                        Reward Points{" "}
+                                    </a>{" "}
+                                    <a
+                                        href="https://opc.webdigify.com/OPC02/OPC037_vesture/index.php?route=account/return"
+                                        className="list-group-item"
+                                    >
+                                        Returns{" "}
+                                    </a>{" "}
+                                    <a
+                                        href="https://opc.webdigify.com/OPC02/OPC037_vesture/index.php?route=account/transaction"
+                                        className="list-group-item"
+                                    >
+                                        Transactions{" "}
+                                    </a>{" "}
+                                    <a
+                                        href="https://opc.webdigify.com/OPC02/OPC037_vesture/index.php?route=account/newsletter"
+                                        className="list-group-item"
+                                    >
+                                        Newsletter{" "}
+                                    </a>
+                                    <a
+                                        href="https://opc.webdigify.com/OPC02/OPC037_vesture/index.php?route=account/logout"
+                                        className="list-group-item"
+                                    >
+                                        Logout{" "}
+                                    </a>
+                                </div>
+                            </div>
+                            <div className="swiper-viewport">
+                                <div id="banner0" className="swiper-container  single-banner ">
+                                    <div className="swiper-wrapper">
+                                        <div className="swiper-slide">
+                                            <a href="#">
+                                                <img
+                                                    src="https://opc.webdigify.com/OPC02/OPC037_vesture/image/cache/catalog/left-banner-272x340.jpg"
+                                                    alt="Left Banner1"
+                                                    className="img-responsive"
+                                                />
+                                            </a>
+                                        </div>
+                                    </div>
+                                    {/* If we need pagination */}
+                                    <div className="swiper-pagination" />
+                                </div>
+                            </div>
+                            <div className="box">
+                                <div className="box-heading">Information</div>
+                                <div className="list-group">
+                                    <a
+                                        className="list-group-item"
+                                        href="https://opc.webdigify.com/OPC02/OPC037_vesture/index.php?route=information/information&information_id=4"
+                                    >
+                                        About Us{" "}
+                                    </a>
+                                    <a
+                                        className="list-group-item"
+                                        href="https://opc.webdigify.com/OPC02/OPC037_vesture/index.php?route=information/information&information_id=6"
+                                    >
+                                        Delivery Information{" "}
+                                    </a>
+                                    <a
+                                        className="list-group-item"
+                                        href="https://opc.webdigify.com/OPC02/OPC037_vesture/index.php?route=information/information&information_id=3"
+                                    >
+                                        Privacy Policy{" "}
+                                    </a>
+                                    <a
+                                        className="list-group-item"
+                                        href="https://opc.webdigify.com/OPC02/OPC037_vesture/index.php?route=information/information&information_id=5"
+                                    >
+                                        Terms &amp; Conditions{" "}
+                                    </a>
+                                    <a
+                                        className="list-group-item"
+                                        href="https://opc.webdigify.com/OPC02/OPC037_vesture/index.php?route=information/contact"
+                                    >
+                                        Contact Us{" "}
+                                    </a>
+                                    <a
+                                        className="list-group-item"
+                                        href="https://opc.webdigify.com/OPC02/OPC037_vesture/index.php?route=information/sitemap"
+                                    >
+                                        Site Map{" "}
+                                    </a>
+                                </div>
+                            </div>
+                        </aside>
+                        <div id="content" className="col-sm-9">
+                            <h1>Order History</h1>
+                            <div className="table-responsive">
+                                <table className="table table-bordered table-hover">
+                                    <thead>
+                                        <tr>
+                                            <td className="text-right">Order ID</td>
+                                            <td className="text-left">Customer</td>
+                                            <td className="text-right">No. of Products</td>
+                                            <td className="text-left">Status</td>
+                                            <td className="text-right">Total</td>
+                                            <td className="text-left">Date Added</td>
+                                            <td />
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td className="text-right">#1</td>
+                                            <td className="text-left">asd asd</td>
+                                            <td className="text-right">1</td>
+                                            <td className="text-left">Pending</td>
+                                            <td className="text-right">$253.00</td>
+                                            <td className="text-left">27 Jan 2025</td>
+                                            <td className="text-right">
+                                                <a
+                                                    href="https://opc.webdigify.com/OPC02/OPC037_vesture/index.php?route=account/order/info&order_id=1"
+                                                    data-toggle="tooltip"
+                                                    title="View"
+                                                    className="btn btn-info"
+                                                >
+                                                    <i className="fa fa-eye" />
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div className="row">
+                                <div className="col-sm-6 text-left" />
+                                <div className="col-sm-6 text-right">
+                                    Showing 1 to 1 of 1 (1 Pages)
+                                </div>
+                            </div>
+                            <div className="buttons clearfix">
+                                <div className="pull-right">
+                                    <a
+                                        href="https://opc.webdigify.com/OPC02/OPC037_vesture/index.php?route=account/account"
+                                        className="btn btn-primary"
+                                    >
+                                        Continue
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
