a192a5a65ce0835ddc774963f892e7583422bfa3
emilbankov <emilbankov17d@gmail.com>
2025-03-01 15:33:12 +0200

    Added images for sweatshirt categories

--- client/src/components/Details/Details.jsx
+++ client/src/components/Details/Details.jsx
@@ -446,7 +446,7 @@ export default function Details() {
                                                 </div>
                                                 <ul className="list-unstyled">
                                                     <li><h4>Цена: {calculatePrice()} лв.</h4></li>
-                                                    <li className="phone"><i className="fas fa-phone"></i>{" "}&nbsp;За бърза поръчка: <a href="tel:+359898739178">+359 898 739 178</a> (08-17ч.)</li>
+                                                    <li className="phone"><i className="fas fa-phone"></i>{" "}&nbsp;За бърза поръчка: <a style={{ color: 'black', fontWeight: 'bold', textDecoration: 'underline' }} href="tel:+359898739178">+359 898 739 178</a> (08-17ч.)</li>
                                                     <li className="rewardpoint"><i className="fas fa-shipping-fast"></i> Безплатна доставка над 100 лв.</li>
                                                     <li className="phone" style={{ display: 'flex', alignItems: 'center' }}>
                                                         <i className="fa-solid fa-shirt"></i> &nbsp;
