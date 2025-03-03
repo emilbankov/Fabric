0621cd151dd6799c9aec10c36f0b2d6c8b84a40a
emilbankov <emilbankov17d@gmail.com>
2025-03-03 18:05:45 +0200

    Added edit account validation

--- client/src/lib/validate.js
+++ client/src/lib/validate.js
@@ -29,6 +29,29 @@ export const registerValidationSchema = Yup.object({
         .required('Задължително поле'),
 });
 
+export const editAccountValidationSchema = Yup.object({
+    firstName: Yup.string()
+        .matches(/^[А-Я][а-я]+$/, 'Името трябва да започва с главна буква и да съдържа само кирилица')
+        .required('Задължително поле'),
+    lastName: Yup.string()
+        .matches(/^[А-Я][а-я]+$/, 'Фамилията трябва да започва с главна буква и да съдържа само кирилица')
+        .required('Задължително поле'),
+    email: Yup.string()
+        .email('Невалиден имейл адрес')
+        .required('Задължително поле'),
+    phoneNumber: Yup.string()
+        .matches(/^8[7-9][0-9] [0-9]{3} [0-9]{3}$/, 'Въведете валиден български телефонен номер')
+        .required('Задължително поле'),
+    address: Yup.string()
+        .required('Задължително поле'),
+    region: Yup.string()
+        .matches(/^[А-Я][а-я]+$/, 'Областта трябва да започва с главна буква и да съдържа само кирилица')
+        .required('Задължително поле'),
+    city: Yup.string()
+        .matches(/^[А-Я][а-я]+$/, 'Градът трябва да започва с главна буква и да съдържа само кирилица')
+        .required('Задължително поле'),
+});
+
 export const addClothingValidationSchema = Yup.object({
     name: Yup.string()
         .required('Задължително поле'),
@@ -128,4 +151,3 @@ export const resetPasswordSchema = Yup.object({
         .oneOf([Yup.ref('password'), null], 'Паролите не съвпадат')
         .required('Задължително поле'),
 });
-
