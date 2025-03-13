079f768108e735829ced6340ea1a9e6d109e4155
emilbankov <emilbankov17d@gmail.com>
2025-03-13 21:48:59 +0200

    Added checkout guard

---
+++ client/src/guards/CheckoutGuard.jsx
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { CartContext } from "../contexts/CartProvider";
+
+export default function CheckoutGuard({ children }) {
+    const { cart } = useContext(CartContext);
+
+    // Redirect to the cart page if the cart is empty
+    if (cart.length === 0) {
+        return <Navigate to="/view-cart" replace />;
+    }
+
+    // Allow access to the Checkout page if the cart is not empty
+    return children;
+} 
