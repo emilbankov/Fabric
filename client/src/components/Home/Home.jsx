4c8ff2755c9d2d4154abdf21960b71e1436887ab
emilbankov <emilbankov17d@gmail.com>
2025-03-14 21:45:19 +0200

    Cleanup home page

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,35 +1,11 @@
-import { useContext, useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import * as clothesService from "../../services/clothesService"
 import { homeCategories, testimonials, typeTranslations } from "../../lib/dictionary";
-import { addToWishlist } from '../../services/authService';
-import AuthContext from "../../contexts/AuthProvider";
-import CustomNotification from "../CustomNotification/CustomNotification";
-import { useWishlist } from "../../contexts/WishlistProvider";
 import MetaTags from '../Meta Tags/MetaTags';
 
 export default function Home() {
     const location = useLocation();
     const navigate = useNavigate();
-    const [newest, setNewest] = useState([]);
-    const [mostSold, setMostSold] = useState([]);
-    const { isAuthenticated } = useContext(AuthContext);
-    const [notification, setNotification] = useState({ message: '', type: '' });
-    const { handleAddToWishlist } = useWishlist();
-
-    useEffect(() => {
-        Promise.all([
-            clothesService.getNewest(),
-            clothesService.getMostSold(),
-        ])
-            .then(([newest, mostSold]) => {
-                setNewest(newest);
-                setMostSold(mostSold);
-            })
-            .catch(err => {
-                console.error("Error fetching data:", err);
-            });
-    }, [location.pathname]);
 
     useEffect(() => {
         const existingScript = document.querySelector('script[src="/js/custom.js"]');
@@ -48,53 +24,12 @@ export default function Home() {
                 script.parentNode.removeChild(script);
             }
         };
-    }, [location.pathname, newest.clothes]);
-
-    const handleWishlistClick = async (id, productName, productType) => {
-        setNotification({ message: '' });
-        setTimeout(() => {
-            handleAddToWishlist(
-                id,
-                () => {
-                    setNotification({ message: '' });
-                    setTimeout(() => {
-                        setNotification({
-                            message: `Успешно добавихте ${typeTranslations[productType]} ${productName} към любими!`,
-                        });
-                    }, 0);
-                },
-                () => {
-                    setNotification({ message: '' });
-                    setTimeout(() => {
-                        setNotification({
-                            message: "Неуспешно добавяне на продукт към любими. Моля, опитайте отново.",
-                        });
-                    }, 0);
-                },
-                () => {
-                    setNotification({ message: '' });
-                    setTimeout(() => {
-                        setNotification({
-                            message: `Този продукт вече е във вашия списък с любими!`,
-                        });
-                    }, 0);
-                },
-                () => {
-                    setNotification({ message: '' });
-                    setTimeout(() => {
-                        setNotification({
-                            message: "Трябва да влезете в профила си, за да добавяте продукти към любими!",
-                        });
-                    }, 0);
-                }
-            );
-        }, 0);
-    };
+    }, [location.pathname]);
 
     return (
         <>
             <MetaTags 
-                title="Начало - Fabric"
+                title="Fabric | Начало"
                 description="Fabric - Вашият онлайн магазин за модерни дрехи и аксесоари. Разгледайте нашите най-нови и най-продавани продукти."
                 keywords="дрехи, дреха, мода, онлайн магазин, тениски, тениска, суичъри, суичър, блузи, блуза, 
                         къси панталони, къси панталон, комплекти, комплект, кърпи, кърпа, бандани, бандана, 
@@ -103,12 +38,6 @@ export default function Home() {
                         blouses, blouse, shorts, short, sets, set, towels, towel, bandanas, bandana, 
                         beach towels, beach towel, accessories, accessory"
             />
-            
-            {notification.message && (
-                <CustomNotification
-                    message={notification.message}
-                />
-            )}
             <div className="content_headercms_bottom" />
             <div className="content-top-breadcum">
                 <div className="container">
